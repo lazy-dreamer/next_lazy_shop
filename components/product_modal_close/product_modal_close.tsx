fdd06307379c49f156403e095a31df663c6d5f4b
Volodymyr <authentic.vvv@gmail.com>
2024-12-11 17:06:34 +0200

    removed usless scrollFix component

--- components/product_modal_close/product_modal_close.tsx
+++ components/product_modal_close/product_modal_close.tsx
@@ -10,10 +10,6 @@ export const ProductModalClose: React.FC<Props> = ({ className = "" }) => {
   const router = useRouter();
   const handleClose = () => {
     router.back();
-    // const previousUrl = document.referrer || "/";
-    // console.log(previousUrl, document.referrer);
-    // console.log("referrer", document.referrer);
-    // router.push(previousUrl, { scroll: false });
   };
   return <button className={"modal_close"} onClick={handleClose} />;
 };
