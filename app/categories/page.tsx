f1602783a8816da8a6ee95508c557c4d379c9011
Volodymyr <authentic.vvv@gmail.com>
2024-12-20 14:12:58 +0200

    category page fix

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -9,7 +9,6 @@ export const metadata: Metadata = {
 export default function Categories() {
   return (
     <>
-      {/* @ts-expect-error Server Component */}
       <CategoriesSection />
     </>
   );
