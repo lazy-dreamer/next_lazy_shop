49d3c2c5e7f225faa0bae043f9209f6d0bd610e4
Volodymyr <authentic.vvv@gmail.com>
2025-01-07 16:48:03 +0200

    category updating issue fix 11

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get("query");
+  console.log("=================query: ", query);
   const categories = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}categories`,
+    `${process.env.NEXT_PUBLIC_API_URL}categories?${query}`,
   );
 
   return NextResponse.json(categories.data);
